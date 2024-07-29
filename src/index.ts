2502909097e46f2ccaed195aed7a22aa0af7b8d4
nick <kobenkao@gmail.com>
2024-07-29 09:30:18 -0700

    expose json as function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {
   DependencyGraphType,
   generateDependencyGraph,
+  generateDependencyGraphJson,
 } from "./generateDependencyGraph";
 import { graphToPrompt } from "./graphToPrompt";
 import { CustomNodesDepsType, FileReferenceType } from "./workflowAPIType";
@@ -11,4 +12,5 @@ export {
   FileReferenceType,
   DependencyGraphType,
   graphToPrompt,
+  generateDependencyGraphJson,
 };
