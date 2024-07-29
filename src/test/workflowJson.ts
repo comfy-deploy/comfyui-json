9d45424f478801bee769fbfbea07730ee40e725b
nick <kobenkao@gmail.com>
2024-07-29 09:20:26 -0700

    clean up test

--- src/test/workflowJson.ts
+++ src/test/workflowJson.ts
@@ -1,11 +1,6 @@
 import workflow_json  from "./workflow.json";
-import workflow_api from "./api.json"
-import { computeCustomNodesMapJson, computeCustomNodesMap } from "../computeCustomNodesMap";
+import { computeCustomNodesMapJson } from "../computeCustomNodesMap";
 import { generateDependencyGraphJson } from "../generateDependencyGraph";
-
-// computeCustomNodesMap ({ workflow_api }).then((deps) => {
-//    console.log(deps) 
-// })
 
 generateDependencyGraphJson({ workflow_json }).then((deps) => {
   console.log(deps)
